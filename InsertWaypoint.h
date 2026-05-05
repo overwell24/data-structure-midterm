6bdf4339ef65294903b87c90a9ab37cd627e4d23
Seonguk Cho <250238039+Seonguk03@users.noreply.github.com>
2026-05-05 16:52:29 +0900

    함수 이름 변경

--- InsertWaypoint.h
+++ InsertWaypoint.h
@@ -9,9 +9,9 @@ typedef struct Waypoint {
 // 리스트의 시작을 나타내는 head 노드를 구조체로 정의
 typedef struct {
 	Waypoint* head;
-} linkedList_h;
+} linkedWaypoint_h;
 
-linkedList_h* createLinkedList_h(void);
-void freeLinkedList_h(linkedList_h* L);
-void printList(linkedList_h* L, FILE* fp);
-void insertLastNode(linkedList_h* L, char c, int x, int y);
+linkedWaypoint_h* createWaypoint_h(void);
+void freeWaypoint_h(linkedWaypoint_h* L);
+void printList(linkedWaypoint_h* L, FILE* fp);
+void insertWaypoint(linkedWaypoint_h* L, char c, int x, int y);
